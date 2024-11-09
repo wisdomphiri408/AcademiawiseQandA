a1b51a987831e11243946ec569d2e9298ec786c0
wisdomphiri408 <wisdomphiri408@gmail.com>
2024-11-09 13:30:57 +0200

    Update main.js

--- main.js
+++ main.js
@@ -3,27 +3,25 @@ const mainSection = document.querySelector('.main-content');
 const filesIcon = document.getElementById('files-icon');
 const savedQAIcon = document.getElementById('saved-QAIcon');
 
-homeIcon.addEventListener('click',function(){
-  mainSection.innerHTML = '';
-  mainSection.innerHTML=`<h3>Welcome to Q and A website</h3>
-  <p>This website will solve almost every school problem you have all you need is to ask</p>
-  `;
-});
 
+function details(h3,p1){
+  mainSection.innerHTML = '';
+  mainSection.innerHTML = `${h3} ${p1}`;
+}
+
+homeIcon.addEventListener('click',function(){
+  details(`<h3>Welcome to Q and A website</h3>`,`  <p>This website will solve almost every school problem you have all you need is to ask</p>`);
+});
 
 filesIcon.addEventListener('click',function(){
-  mainSection.innerHTML = '';
-  mainSection.innerHTML=`<h3>You can download documents here for free</h3>
-  <p>all you need is scroll through the available books, but life is easier with a search bar, so search the document</p>
-  `;
+  details(`<h3>You can download documents here for free</h3>`,`  <p>all you need is scroll through the available books, but life is easier with a search bar, so search the document</p>`);
 });
 
+
 savedQAIcon.addEventListener('click',function(){
-  mainSection.innerHTML = '';
-  mainSection.innerHTML=`<h3>You will find all your saved questions and answers here</h3>
-  <p>
+  details(`<h3>You will find all your saved questions and answers here</h3>`,`<p>
   Why search for a question and answer you saw instead of just saving it for quick overview
-  </p>
-  `;
+  </p>`);
 });
+
 
